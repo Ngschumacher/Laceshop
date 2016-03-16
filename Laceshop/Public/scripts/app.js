eacf12c36af2723da88dd2b4362295f93ef8af78
Ngschumacher <niclas@schu.dk>
2016-03-16 22:41:03 +0100

    added typescript

--- Laceshop/Public/scripts/app.js
+++ Laceshop/Public/scripts/app.js
@@ -1,46 +1,100 @@
-angular.module('storeApp', [])
-
-	.controller('cartController', function($scope) {
-        console.log("test");
-	$scope.tax = 20;
-	$scope.invoice = {
-		items: [{
-				qty: 1,
-				title: 'Tuscan urns',
-				cost: 14,
-				url: 'product1'},
-				{qty: 1,
-				title: 'Bosphorus bowls',
-				cost: 28,
-				url: 'product2'},
-				{qty: 2,
-				title: 'Langdon vases ',
-				cost: 24,
-				url: 'product3'}
-				]
-	};
-
-	$scope.removeItem = function(index) {
-		$scope.invoice.items.splice(index, 1);
-	},
-
-	$scope.total = function() {
-		var total = 0;
-		angular.forEach($scope.invoice.items, function(item) {
-				total += item.qty * item.cost;
-		})
-		return total;
-	}		
-
-	$scope.shiping = function() {
-		var shiping = 0;
-
-		if($scope.total() < 100) {
-			shiping = 25;
-		} else {
-			shiping = 0;
-		}
-		return shiping;
-	}
-
-});
+//import Util = require("util");
+//class NgcAppComponent {
+//    public static selector = 'ngc-app';
+//    public static template = `<div>{{ ctrl.message }}</div>`;
+//    private message;
+//    constructor( @Util.Inject('$log') private $log: angular.ILogService) {
+//        this.$log.info('Hello Component!');
+//        this.message = 'Some message to display';
+//    }
+//}
+//console.log("hej");
+//var phonecatApp = angular.module('storeApp', []);
+//class CartController {
+//    constructor($scope) {
+//        $scope.tax = 20;
+//        $scope.invoice = {
+//            items: [{
+//                qty: 1,
+//                title: 'Tuscan urns',
+//                cost: 14,
+//                url: 'product1'
+//            },
+//                {
+//                    qty: 1,
+//                    title: 'Bosphorus bowls',
+//                    cost: 28,
+//                    url: 'product2'
+//                },
+//                {
+//                    qty: 2,
+//                    title: 'Langdon vases ',
+//                    cost: 24,
+//                    url: 'product3'
+//                }
+//            ]
+//        };
+//    }
+//};
+//phonecatApp.controller("CartController", new CartController)
+var app;
+(function (app) {
+    var controllers;
+    (function (controllers) {
+        var BasketController = (function () {
+            function BasketController() {
+                this.items = [
+                    {
+                        qty: 1,
+                        title: 'Tuscan urns',
+                        cost: 14,
+                        url: 'product1'
+                    },
+                    {
+                        qty: 1,
+                        title: 'Bosphorus bowls',
+                        cost: 28,
+                        url: 'product2'
+                    },
+                    {
+                        qty: 2,
+                        title: 'Langdon vases ',
+                        cost: 24,
+                        url: 'product3'
+                    }
+                ];
+            }
+            return BasketController;
+        }());
+    })(controllers = app.controllers || (app.controllers = {}));
+})(app || (app = {}));
+var app;
+(function (app) {
+    var directives;
+    (function (directives) {
+        var SomeComponentController = (function () {
+            function SomeComponentController() {
+                this.textBinding = '';
+                this.dataBinding = 0;
+            }
+            SomeComponentController.prototype.add = function () {
+                this.functionBinding();
+            };
+            return SomeComponentController;
+        }());
+        var SomeComponent = (function () {
+            function SomeComponent() {
+                this.bindings = {
+                    textBinding: '@',
+                    dataBinding: '<',
+                    functionBinding: '&'
+                };
+                this.controller = SomeComponentController;
+                this.templateUrl = 'some-component.html';
+            }
+            return SomeComponent;
+        }());
+        angular.module('storeApp', []).component('someComponent', new SomeComponent());
+    })(directives = app.directives || (app.directives = {}));
+})(app || (app = {}));
+//# sourceMappingURL=app.js.map
